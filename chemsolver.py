35967d2868f015cbcf2b71f4c274340ec9dac2bc
David Carroll <carroll.david.s@gmail.com>
2023-04-03 00:01:19 -0500

    Use a function for adding reactants and products to the dictionaries

--- chemsolver.py
+++ chemsolver.py
@@ -1,26 +1,20 @@
 from chempy import balance_stoichiometry
 
-# eq = '12 C4H9Cl + O2 -> CO2 + H2O + Cl2'
+def create_dict_from_side(side):
+    dct = {}
+    for r in side.split('+'):
+        split_r = r.split(None, 1)
+        if len(split_r) == 2:
+            coeff, name = split_r
+            dct[name.strip()] = int(coeff.strip())
+        elif len(split_r) == 1:
+            dct[split_r[0].strip()] = 1
+    return dct
+
 eq = input("Enter the chemical equation to balance (Sample format: 12 C4H9Cl + O2 -> CO2 + H2O + Cl2): ")
-eq = eq.strip() # Strip extra white spaces
-print(repr(eq))
 reactants, products = eq.split('->')
-reactant_dict = {}
-for r in reactants.split('+'):
-    split_r = r.split(None, 1)
-    if len(split_r) == 2:
-        coeff, name = split_r
-        reactant_dict[name.strip()] = int(coeff.strip())
-    else:
-        reactant_dict[r.strip()] = 1
-product_dict = {}
-for p in products.split('+'):
-    split_p = p.split(None, 1)
-    if len(split_p) == 2:
-        coeff, name = split_p
-        product_dict[name.strip()] = int(coeff.strip())
-    else:
-        product_dict[p.strip()] = 1
+reactant_dict = create_dict_from_side(reactants)
+product_dict = create_dict_from_side(products)
 print(reactant_dict)
 print(product_dict)
 reactants, products = balance_stoichiometry(reactant_dict, product_dict)
